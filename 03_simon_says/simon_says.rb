08b7a9569fbb578cc3139777ce4a92e8ce564f5d
Remnace <rileyae20@gmail.com>
2016-09-09 11:11:52 -0500

    03 complete.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,45 @@
 #write your code here
+def echo(a)
+	return a
+end
+
+def shout(a)
+	return a.upcase
+end
+
+def repeat(a, b=nil)
+	if b == nil then
+		return a + ' ' + a
+	end
+	return [a] * b * ' '
+end
+
+def start_of_word(a, b)
+	a.split('')
+	c = 0
+	toReturn = ''
+	while c < b do
+		toReturn += a[c]
+		c+=1
+	end
+	return toReturn
+end
+
+def first_word(a)
+	words = a.split(/\W+/)
+	return words[0]
+end
+
+def titleize(a)
+	words = a.split(/\W+/)
+	check = words[0]
+	check[0] = check[0].upcase
+	words[0] = check
+	for word in words
+		if word == 'and' || word == 'over' || word == 'the' then
+			next
+		end
+		word[0] = word[0].upcase
+	end
+	return words.join(' ')
+end
